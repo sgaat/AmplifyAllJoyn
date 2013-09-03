00a95774d8f1152077c79b7103ecd729a5151499
Sucharita Gaat <sgaat@amplify.com>
2013-09-03 09:42:31 -0400

    Deleting chat project

--- chat/gen/org/alljoyn/bus/sample/chat/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.alljoyn.bus.sample.chat;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- chat/src/org/alljoyn/bus/sample/chat/Point.java
+++
@@ -1,8 +0,0 @@
-package org.alljoyn.bus.sample.chat;
-
-public class Point {
-	public float x, y;
-	public float oldX=-1.0f, oldY=-1.0f;
-	public boolean sent;
-	public int colorCode=0xff000000;
-}
